918be5b269cdf26c08a291b9a3b3c89d09a20638
otr1v <100768041+otr1v@users.noreply.github.com>
2024-04-01 12:24:18 +0300

    fixed bugs

--- hw3/impulse.v
+++ hw3/impulse.v
@@ -1,31 +1,31 @@
-module impulse5(
+module impulse(
    input wire clk, 
    input wire reset,
-   output reg impulse
+   output wire impulse
 );
 reg[2:0] count;
-
+reg imp;
 always @(posedge clk)
 begin
 	if (reset)
 	begin
 		count <= 3'd0;
-		impulse <= 1'd0;
+		imp <= 1'd0;
 	end
 	else
 		begin
-		if (count == 3'h5)
+		if (count == 3'h4)
 			begin 
 			count <= 3'h0;
-			impulse <= 1'd0;
+			imp <= 1'h1;
 			end
 		else
 			begin
 				count <= count + 3'h1;
-				impulse <= 1'd0;
+				imp <= 1'h0;
 			end
 		end		
 end
 
-assign impulse = (count == 3'h4);
+assign impulse = imp;
 endmodule
